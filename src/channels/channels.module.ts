ed0b1a8e74423d886dca40b28eeb393354fabf74
Mahdi Vajdi <mahdivajdii@gmail.com>
2023-10-17 23:19:24 +0330

    feat: create default team upon channel creation

--- src/channels/channels.module.ts
+++ src/channels/channels.module.ts
@@ -5,11 +5,13 @@ import { ChannelsService } from './channels.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Channel, ChannelsSchema } from './models/channel.schema';
 import { OperatorsModule } from 'src/operators/operators.module';
+import { TeamsModule } from 'src/teams/teams.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Channel.name, schema: ChannelsSchema }]),
     OperatorsModule,
+    TeamsModule,
   ],
   controllers: [ChannelsController],
   providers: [ChannelsService, ChannelsRepository],
